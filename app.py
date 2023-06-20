90668d09664e29e9c0242662224a2c9d6e62f60e
JuliaRoughley <roughleyjulia@gmail.com>
2023-06-20 13:32:14 +0100

    changes to add in the add.html file for form creation to add a new blog post, and the /add route that can process both GET and POST requests

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import json
 
 app = Flask(__name__)
@@ -9,6 +9,19 @@ def index():
     with open('storage.json', 'r') as file:
         blog_posts = json.load(file)
     return render_template('index.html', posts=blog_posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        content = request.form['content']
+
+        print(f"New Blog Post: Title - {title}, Author - {author}, Content - {content}")
+        return "Blog post added successfully!"
+
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
